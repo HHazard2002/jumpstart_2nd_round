48fa184408e99905f22125cd06b1bd79d1740f10
HHazard2002 <hugo7.hazard@gmail.com>
2024-05-18 20:52:09 +0100

    updated layout

--- frontend/src/components/candidate_card.js
+++ frontend/src/components/candidate_card.js
@@ -23,7 +23,7 @@ function CandidateCard({ candidate, isInList, onToggleCandidate }) {
                 src={linkedin}
                 alt={candidate.name}
                 title="Linkedin icon"
-                className="h-6 w-6 ml-2"
+                className="h-5 w-5 ml-2"
                 onClick={() => {
                   window.open(candidate.linkedin, "_blank");
                 }}
